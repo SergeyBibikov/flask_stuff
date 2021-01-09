706257f6a26f521b45bfd539dd5aa4623b569d37
Sergey Bibikov <sergey.bibikov.92@mail.ru>
2021-01-09 12:16:33 +0300

    forms

--- hello.py
+++ hello.py
@@ -1,20 +1,33 @@
 import os
-from flask import Flask
-from flask import request
-from flask import make_response
-from flask import render_template
+from flask import Flask,request,make_response,render_template
+from flask import redirect,session,url_for,flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
 from datetime import datetime
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ(['SK'])
+app.config['SECRET_KEY'] = os.getenv('SK')
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-@app.route("/")
+
+class NameForm(FlaskForm):
+    name = StringField("What's your name?",validators=[DataRequired(message="Enter you name, please")])
+    submit = SubmitField("Submit")
+
+@app.route("/",methods = ["GET","POST"])
 def index():
-    return render_template("index.html",current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name!=form.name.data:
+            flash(f"Name changed from {old_name} to {form.name.data}")
+        session['name'] = form.name.data
+        return redirect(url_for("index"))
+    return render_template("index.html", form=form, name=session.get('name'), current_time=datetime.utcnow())
 
 @app.route("/user/<name>")
 def name(name):
@@ -30,6 +43,12 @@ def list():
 @app.route("/sub")
 def sub():
     return render_template("subbase.html")
+
+@app.route("/clear")
+def clear():
+    print(help(session))
+    session['name'] = None
+    return redirect(url_for("index"))
 
 @app.errorhandler(404)
 def not_found(e):
